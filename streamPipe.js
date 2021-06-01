be88e331e436036a819a881a832d4e12b6a91de4
Bapita <bapitaroy14@gmail.com>
2021-06-01 20:51:49 +0530

    Node Core methods

--- streamPipe.js
+++ streamPipe.js
@@ -1,4 +1,16 @@
 /*
-    stream.pipe() --> this method is used to take a readable stream and 
+    stream.pipe(destination, [options]) --> this method is used to take a readable stream and 
     connect it to a writable stream
 */
+
+const fs = require('fs');
+const http = require('http');
+const server = http.createServer();
+
+server.on('request', (req, res)=> {
+
+    const rstream = fs.createReadStream("aSync.txt");
+    rstream.pipe(res)
+})
+
+server.listen(8000,"127.0.0.1")
