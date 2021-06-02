fc0b0dc33c57ecf60479b8037f6a2377f725428e
Bapita <bapitaroy14@gmail.com>
2021-06-02 17:32:25 +0530

    node JS weather app

---
+++ NodeWeatherApp/index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html","utf-8");
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp/10);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min/10);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max/10);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+    return temperature;
+}
+const server = http.createServer((req,res) => {
+    if(req.url == "/"){
+        // nodeJS streams
+        requests('http://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=dbe7ec7fd358d632555d8a6d51fb087d')
+        .on('data',(chunk) => {
+            const objData = JSON.parse(chunk);
+            const arrData = [objData]; // array of an object
+          //console.log(arrData[0].main.temp)
+            const realTimeData = arrData.map((val)=> replaceVal(homeFile, val)).join("");
+            res.write(realTimeData);
+        })
+        .on('end', (err) => {
+          if (err) return console.log('connection closed due to errors', err);
+         
+         res.end();
+        });
+    }
+})
+
+server.listen(8000,"127.0.0.1");
